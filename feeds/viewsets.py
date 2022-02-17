3d3d0b560d4ed66d050885f16b5487189a175120
Kishu Maharjan <kishu.maharjan@infodevelopers.com.np>
2022-02-17 17:36:02 +0545

    fix: update view

--- feeds/viewsets.py
+++ feeds/viewsets.py
@@ -3,29 +3,38 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import *
 from .mixins import *
 
+
 # Create your views here.
-
-
 class PostImageViewSet(ModelViewSet):
+    """ View sets for Post Images """
+
     serializer_class = PostImageSerializer
     queryset = PostImage.objects.all()
 
 
 class LikeViewSet(ModelViewSet):
+    """ View sets for Post Likes """
+
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
 
 
 class CommentViewSet(ModelViewSet):
+    """ View sets for Post Comments """
+
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
 
 
 class PostViewSet(ModelViewSet, PostDeleteMixin):
+    """ View sets for Posts """
+
     serializer_class = PostSerializer
     queryset = Post.objects.all()
 
 
-class GetPostViewSet(ModelViewSet):
+class GetPostViewSet(ModelViewSet, HomeFeedPostsListMixin):
+    """ View sets to get Feed Posts in Homepage """
+
     serializer_class = GetPostSerializer
     queryset = Post.objects.all()
