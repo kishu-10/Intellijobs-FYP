8f4c3fb4fd7c6bc8cd72c1deff7180785a642494
Kishu Maharjan <kishu.maharjan@infodevelopers.com.np>
2022-02-18 14:03:40 +0545

    fix: address command

--- users/management/commands/create_district.py
+++ users/management/commands/create_district.py
@@ -10,12 +10,14 @@ class Command(BaseCommand):
     help = 'create countries to the database table from csv file'
 
     def handle(self, *args, **options):
-        with open(os.path.join(settings.BASE_DIR, 'fixtures/country.csv'), 'r') as file:
+        with open(os.path.join(settings.BASE_DIR, 'fixtures/district.csv'), 'r') as file:
             reader = csv.reader(file)
             next(reader)  # skip first line
             for row in reader:
-                province = Province.objects.get(province_code=row[1]) 
-                district, created = District.objects.get_or_create(
-                    name=row[0],
-                    province=province.id,
-                )
+                province = Province.objects.filter(
+                    province_code=row[1]).first()
+                if province:
+                    district, created = District.objects.get_or_create(
+                        name=row[0],
+                        province=province,
+                    )
